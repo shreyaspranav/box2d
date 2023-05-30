4377b8623cfc2e7250cc6b77b50f9cbe1c5dfb0b
Shreyas Pranav <shrepranav5@gmail.com>
2023-05-30 22:59:37 +0530

    Added premake script

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "Box2D"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
+
+    targetdir ("%{wks.location}/bin/" .. bin_folder .. "/") -- make a variable called "bin_folder" in main premake5.lua file
+    objdir ("%{wks.location}/bin/" .. bin_folder .. "/obj")
+
+    files {
+        "src/**.cpp",
+        "src/**.h",
+    }
+    includedirs {
+        "include"
+    }
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release_Debug"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Production"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
